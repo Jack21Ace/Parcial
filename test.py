c882cf2dc686659a761cf10645e40fb0ec050b06
Jack21Ace <djherrera89639@umanizales.edu.co>
2022-03-15 19:56:42 -0500

    one try

--- test.py
+++ test.py
@@ -1,32 +1,29 @@
-from enum import Enum
-#from typing import MutableMapping
+# Numeros a validar
+def capt_num():
+    n: int = int(input("Cantidad de elementos a validar"))
+    if n <= 0:
+        print("Debe ser un numero mayor a 0")
+        capt_num()
+    else:
+        num_list = np.random.randint(1, n, n)
+    return validacion(num_list)
 
-# Enumeración Eps
-class Eps(Enum):
-    ASMETSALUD = "Asmet Salud"
-    SURA = "Sura"
-    MEDIMAS = "Medimas"
-    SALUDMORTAL = "Salud Mortal"
 
-# Enumeración RH
-class Rh(Enum):
-    O_POSITIVE = 'O+'
-    O_NEGATIVE = 'O-'
-    A_POSITIVE = 'A+'
-    A_NEGATIVE = 'A-'
-    B_POSITIVE = 'B-'
-    B_NEGATIVE = 'B+'
-    AB_POSITIVE = 'AB+'
-    AB_NEGATIVE = 'AB-'
-
-class User:
-    def __init__(self, nombre:str, apellido:str, cc:str, edad:int, rh:Rh, eps:Eps):
-        self.__nombre = nombre
-        self.__apellido = apellido
-        self.__cc = cc
-        self.__edad = edad
-        self.__rh = rh
-        self.__eps = eps
-
-    def __str__(self) -> str:
-        pass
+def validacion(num_list):
+    muestra: list = num_list
+    print(f"A continuación se presenta la muestra a examinar: \n{[muestra]}")
+    for e in muestra:
+        if e < 0:
+            print("Factorial of negative e does not exist")
+        elif e == 0:
+            return 1
+        else:
+            fact = 1
+            while(e > 1):
+                fact *= e
+                e -= 1
+            return fact
+    if fact % 2 != 0:
+        print(f"El numero {e} es un numero inteligente")
+    else:
+        print(f"El numero {e} no un numero inteligente")
